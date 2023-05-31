e7939142f7464cb1dfc9d9ade9ed38006107ca18
MiimeSox <kontakt@malteschumacher.de>
2023-05-31 09:27:35 +0200

    Initial Commit

---
+++ ProgressRingView/ProgressRingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressRingViewApp.swift
+//  ProgressRingView
+//
+//  Created by Malte Schumacher on 31.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressRingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
